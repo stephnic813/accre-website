04a42a289becdc228dcee6be168e130f6bb5ef76
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-07-13 22:57:24 +0800

    Add hamburger and chevron down icon

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.querySelector("header").style.height = "15vh";
-    document.querySelector(".launch-main-title").style.fontSize = "2.5em";
-    document.querySelector(".launch-main-course").style.fontSize = "2.15em";
-    document.querySelector(".launch-main-quote").style.fontSize = "1.5em";
-  } else {
-    document.querySelector("header").style.height = "100vh";
-    document.querySelector(".launch-main-title").style.fontSize = "3em";
-    document.querySelector(".launch-main-course").style.fontSize = "2.6em";
-    document.querySelector(".launch-main-quote").style.fontSize = "2em";  
-  }
+  // if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  //   document.querySelector("header").style.height = "15vh";
+  //   document.querySelector(".launch-main-title").style.fontSize = "2.5em";
+  //   document.querySelector(".launch-main-course").style.fontSize = "2.15em";
+  //   document.querySelector(".launch-main-quote").style.fontSize = "1.5em";
+  // } else {
+  //   document.querySelector("header").style.height = "100vh";
+  //   document.querySelector(".launch-main-title").style.fontSize = "3em";
+  //   document.querySelector(".launch-main-course").style.fontSize = "2.6em";
+  //   document.querySelector(".launch-main-quote").style.fontSize = "2em";  
+  // }
 }
 
 // function scrollFunction() {
