ba496c0affe2df69b5be9cda04e647d31e93b160
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-07-13 22:31:57 +0800

    launch-page

---
+++ index.js
@@ -0,0 +1,28 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector("header").style.height = "15vh";
+    document.querySelector(".launch-main-title").style.fontSize = "2.5em";
+    document.querySelector(".launch-main-course").style.fontSize = "2.15em";
+    document.querySelector(".launch-main-quote").style.fontSize = "1.5em";
+  } else {
+    document.querySelector("header").style.height = "100vh";
+    document.querySelector(".launch-main-title").style.fontSize = "3em";
+    document.querySelector(".launch-main-course").style.fontSize = "2.6em";
+    document.querySelector(".launch-main-quote").style.fontSize = "2em";  
+  }
+}
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//       document.querySelector(".launch-main-title").style.fontSize = "2.5em";
+//       document.querySelector(".launch-main-course").style.fontSize = "2.15em";
+//       document.querySelector(".launch-main-quote").style.fontSize = "1.5em";
+//     } else {
+//       document.querySelector(".launch-main-title").style.fontSize = "3em";
+//       document.querySelector(".launch-main-course").style.fontSize = "2.6em";
+//       document.querySelector(".launch-main-quote").style.fontSize = "2em";
+      
+//     }
+//   }
