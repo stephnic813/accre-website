3f1c54d6430a68f02c1db8e445dc6fc810db24ab
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-07-13 23:20:35 +0800

    header-prog

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    
+    document.querySelector("header").style.height= "25vh";
     document.querySelector(".launch-main-title").style.fontSize = "2.5em";
     document.querySelector(".launch-main-course").style.fontSize = "2.15em";
     document.querySelector(".launch-main-quote").style.fontSize = "1.5em";
   } else {
-    
+    document.querySelector("header").style.height= "100vh";
     document.querySelector(".launch-main-title").style.fontSize = "3em";
     document.querySelector(".launch-main-course").style.fontSize = "2.6em";
     document.querySelector(".launch-main-quote").style.fontSize = "2em";  
