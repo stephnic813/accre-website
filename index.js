1db60a1fb6111ba05d32f165790baed8ef0283fd
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2023-07-14 02:45:54 +0800

    Launch and Home Working

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (scrollY > 3) {
+  if (scrollY > 1) {
     document.querySelector("header").style.height= "20vh";
     document.querySelector("header").style.flexDirection = "row";
     document.querySelector(".home-arrow-down").style.display = "none";
-    document.querySelector(".launch-main-title").style.fontSize = "2em";
+    document.querySelector(".launch-main-title").style.fontSize = "2.2em";
     document.querySelector(".launch-main-course").style.fontSize = "1.75em";
     document.querySelector(".launch-main-quote").style.fontSize = "1.2em";
   } else {
