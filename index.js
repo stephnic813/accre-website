be0e30f4f04507472a11b7abb48be430af319d16
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-07-15 21:36:17 +0800

    Create template.html for header

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (scrollY > 1) {
-    document.querySelector("header").style.height= "20vh";
-    document.querySelector("header").style.flexDirection = "row";
+    document.querySelector("header.header-home").style.height= "20vh";
+    document.querySelector("header.header-home").style.flexDirection = "row";
     document.querySelector(".home-arrow-down").style.display = "none";
-    document.querySelector(".launch-main-title").style.fontSize = "2.2em";
-    document.querySelector(".launch-main-course").style.fontSize = "1.75em";
-    document.querySelector(".launch-main-quote").style.fontSize = "1.2em";
+    document.querySelector("header.header-home .launch-main-title").style.fontSize = "2.2em";
+    document.querySelector("header.header-home .launch-main-course").style.fontSize = "1.75em";
+    document.querySelector("header.header-home .launch-main-quote").style.fontSize = "1.2em";
   } else {
-    document.querySelector("header").style.height= "100vh";
-    document.querySelector("header").style.flexDirection = "column";
-    document.querySelector(".home-arrow-down").style.display = "block";
-    document.querySelector(".launch-main-title").style.fontSize = "3em";
-    document.querySelector(".launch-main-course").style.fontSize = "2.6em";
-    document.querySelector(".launch-main-quote").style.fontSize = "2em";  
+    document.querySelector("header.header-home").style.height= "100vh";
+    document.querySelector("header.header-home").style.flexDirection = "column";
+    document.querySelector("header.header-home > .home-arrow-down").style.display = "block";
+    document.querySelector("header.header-home .launch-main-title").style.fontSize = "3em";
+    document.querySelector("header.header-home .launch-main-course").style.fontSize = "2.6em";
+    document.querySelector("header.header-home .launch-main-quote").style.fontSize = "2em";  
   }
 }
 
